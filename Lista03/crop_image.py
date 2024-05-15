ee3d960e93d9539d76f144214d9afd81951f3e4c
TiagoCAAmorim <tiago.ca.amorim@gmail.com>
2024-05-15 16:17:04 -0300

    Report with MLP and CNN

--- Lista03/crop_image.py
+++ Lista03/crop_image.py
@@ -20,3 +20,27 @@ crop(path, new_path)
 path = ".\Lista03\Report\png\ResNet_history.png"
 new_path = ".\Lista03\Report\png\ResNet_history_cropped.png"
 crop(path, new_path)
+
+def upper_crop(image_path, new_path):
+    img = Image.open(image_path)
+    width, height = img.size
+    left, right = 0, width
+    upper, lower = 0, height*0.517
+    cropped_img = img.crop((left, upper, right, lower))
+    cropped_img.save(new_path)
+
+def lower_crop(image_path, new_path):
+    img = Image.open(image_path)
+    width, height = img.size
+    left, right = 0, width
+    upper, lower = height*0.515, height
+    cropped_img = img.crop((left, upper, right, lower))
+    cropped_img.save(new_path)
+
+path = ".\Lista03\Report\png\ResNet_model.png"
+new_path = ".\Lista03\Report\png\ResNet_model_upper.png"
+upper_crop(path, new_path)
+
+path = ".\Lista03\Report\png\ResNet_model.png"
+new_path = ".\Lista03\Report\png\ResNet_model_lower.png"
+lower_crop(path, new_path)
